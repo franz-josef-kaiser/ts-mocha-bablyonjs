6487b5657d56acf891e9d1a81e4cac7ceaddc42d
Franz Josef Kaiser <fj.kaiser@cyledge.com>
2021-06-15 16:18:15 +0200

    Initial commit

---
+++ tests/index.spec.ts
@@ -0,0 +1,33 @@
+import * as chai from 'chai';
+const { expect } = chai
+import { describe, before, it } from 'mocha';
+
+/* 1) "SyntaxError: Named export 'Camera' not found. The requested module '@babylonjs/core/Cameras/camera.js'
+	     is a CommonJS module, which may not support all module.exports as named exports.
+
+	     CommonJS modules can always be imported via the default export, for example using:
+
+	     import pkg from '@babylonjs/core/Cameras/camera.js';
+	     const { Camera } = pkg;
+ */
+import { Camera } from '@babylonjs/core/Cameras/camera.js';
+
+/* 2) "SyntaxError: Cannot use import statement outside a module" */
+//import camera from '@babylonjs/core/Cameras/camera.js';
+//const { Camera } = camera;
+
+
+describe('Self sustained', () => {
+	// Test to confirm that tests work.
+	it('should equal a string with a string', () => {
+		expect('789')
+			.to.equal('789')
+			.and.to.be.a('string')
+	})
+})
+
+describe('The Camera', () => {
+	it('should get logged to the console', () => {
+		console.info(Camera)
+	})
+})
